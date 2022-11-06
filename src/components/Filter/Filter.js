94343d945aa4d5c61432a65cade63c035047a837
Alona <103571003+ARomanii@users.noreply.github.com>
2022-11-06 20:23:33 +0200

    Initial commit

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Filter = ({ onFilter }) => (
+  <>
+    <p>Find contacts by name</p>
+    <input type="text" onChange={onFilter} name="filter"></input>
+  </>
+);
+
+export default Filter;
+
+Filter.propTypes = {
+  onFilter: PropTypes.func.isRequired,
+};
